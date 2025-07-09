3cbcfe0dc11bfb9f64fc50888f18bb1b75b82c85
colorbled <colorbled@gmail.com>
2025-07-09 10:50:55 -0500

    Added path to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'node:fs';
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite'
 
 function noJekyllPlugin() {
