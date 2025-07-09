fdcf62a66eaff2446bc72ade2a1eb8eebfa8ad04
colorbled <colorbled@gmail.com>
2025-07-09 10:37:04 -0500

    Added nojekyll

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+function noJekyllPlugin() {
+    return {
+        name: 'nojekyll',
+        closeBundle: () => {
+            fs.writeFileSync(resolve(__dirname, 'dist/.nojekyll'), '');
+        }
+    };
+}
+
 export default defineConfig({
     base: "/RebeccaBegis/",
     plugins: [
       react(),
+      noJekyllPlugin(),
       tailwindcss(),
     ],
 })
